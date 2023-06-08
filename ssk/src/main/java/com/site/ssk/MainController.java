dbd9e885513da6cdc5a035cb4cc4569a32b8b5b6
sooobb <113891244+sooobb@users.noreply.github.com>
2023-06-08 18:40:58 +0900

    Add files via upload

--- ssk/src/main/java/com/site/ssk/MainController.java
+++ ssk/src/main/java/com/site/ssk/MainController.java
@@ -40,6 +40,7 @@ public class MainController {
 	@PostMapping("/translate")
 	public String create(@RequestBody Map<String,String> requestData) {
 		
+		// 번역 요청이 들어오면, json에 있던 정보를 저장
 		translate t = new translate();
 		
 		requestData.forEach((key, value) -> {
@@ -64,31 +65,57 @@ public class MainController {
                 .withRegion("ap-northeast-2")
                 .build();
         // String text = "Anti-circumvention refers to laws which prohibit the circumvention of technological barriers for using a digital good in certain ways which the rightsholders do not wish to allow.";
+        // 서버야 번역해줘
         TranslateTextRequest request3 = new TranslateTextRequest()
                 .withText(t.Text)
                 .withTerminologyNames(t.TerminologyNames)
                 .withSourceLanguageCode(t.SourceLanguageCode)
                 .withTargetLanguageCode(t.TargetLanguageCode);
         TranslateTextResult result3  = translate.translateText(request3);
-        System.out.println(result3.getTranslatedText());
+        // result3이 순수하게 번역된 문장
+        System.out.println("그냥번역: " + result3.getTranslatedText());
         
-        if (result3.getAppliedTerminologies().size()!=0 && t.SourceLanguageCode=="ko") { 
-        	System.out.println(result3.getAppliedTerminologies().get(0).getTerms().get(0).getSourceText());
-            String term = result3.getAppliedTerminologies().get(0).getTerms().get(0).getSourceText();
-        	String targetTerm = result3.getAppliedTerminologies().get(0).getTerms().get(0).getTargetText();
-            String modified = t.Text.replace(term, '[' + term + ']');
-            System.out.println(modified);
-            TranslateTextRequest modifiedRequest = new TranslateTextRequest()
-                    .withText(modified)
-                    .withTerminologyNames(t.TerminologyNames)
-                    .withSourceLanguageCode(t.SourceLanguageCode)
-                    .withTargetLanguageCode(t.TargetLanguageCode);
-            TranslateTextResult modifiedResult = translate.translateText(modifiedRequest);
-            String modifiedText = modifiedResult.getTranslatedText();
-            int targetIndex = modifiedText.indexOf(targetTerm); 
-            String result = modifiedText.substring(0,targetIndex-1) + targetTerm + modifiedText.substring(targetIndex + targetTerm.length() +2);
-            return result;
+       
+        int termsize = result3.getAppliedTerminologies().get(0).getTerms().size();
+        System.out.println("termsize : " + termsize);
+        
+        for(int i = 0; i < termsize ; i++) {
+        	String source = result3.getAppliedTerminologies().get(0).getTerms().get(i).getSourceText();
+        	String target = result3.getAppliedTerminologies().get(0).getTerms().get(i).getTargetText();
+        	System.out.println(source + target);
         }
-        else return result3.getTranslatedText();
+        
+        // modified는 순수번역 문장에서 
+        String modified = t.Text;
+        if(t.SourceLanguageCode.equals("en")) {
+        	System.out.println("영어를 한국어로");
+        	
+        	if (result3.getAppliedTerminologies().get(0).getTerms().size()!=0) { 
+            	System.out.println("fadfasfasdfasfsdafasdfasdfasdf");
+            	String term = "";
+            	String targetTerm = "";
+            	for(int i = 0; i < termsize ; i++) {
+                	term = result3.getAppliedTerminologies().get(0).getTerms().get(i).getSourceText();
+                	targetTerm = result3.getAppliedTerminologies().get(0).getTerms().get(i).getTargetText();
+                	System.out.println(targetTerm);
+                    modified = t.Text.replace(term, '[' + term + ']');
+                    System.out.println("modified:"+modified);
+                }
+
+                TranslateTextRequest modifiedRequest = new TranslateTextRequest()
+                        .withText(modified)
+                        .withTerminologyNames(t.TerminologyNames)
+                        .withSourceLanguageCode(t.SourceLanguageCode)
+                        .withTargetLanguageCode(t.TargetLanguageCode);
+                
+                TranslateTextResult modifiedResult = translate.translateText(modifiedRequest);
+                String modifiedText = modifiedResult.getTranslatedText();
+                int targetIndex = modifiedText.indexOf(targetTerm); 
+                String result = modifiedText.substring(0,targetIndex-1) + targetTerm + modifiedText.substring(targetIndex + targetTerm.length()+2);
+                System.out.println("result : "+ result);
+                return result;
+            } else return result3.getTranslatedText();
+        } else return result3.getTranslatedText();
+            
 	}
 }
